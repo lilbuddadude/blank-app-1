f2e192d2441c6f59b4229cb244774ba2f1e2b92a
lilbuddadude <andrewdjenkins97@gmail.com>
2025-02-25 10:32:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
-import streamlit as st
 
-st.title("🎈 My new Streamlit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 import requests
 import pandas as pd
 import numpy as np
